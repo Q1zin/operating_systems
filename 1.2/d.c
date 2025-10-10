23ea3a432d09a06aaebe8005bc2d459a087adf52
Vladimir <q1zin@mail.ru>
2025-10-10 12:06:11 +0700

    complite lab 1.2

---
+++ 1.2/d.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define SUCCESS 0
+#define ERROR 1
+
+void *mythread() {
+    printf("mythread [tid: %d]: Hello from mythread!\n", gettid());
+    return NULL;
+}
+
+int main() {
+    pthread_t tid;
+    int err;
+    int threads_num = 0;
+
+    printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+    while (true) {
+        err = pthread_create(&tid, NULL, mythread, NULL);
+        if (err) {
+            printf("main: pthread_create() failed: %s\n", strerror(err));
+            break;
+        }
+        threads_num++;
+    }
+
+    printf("main [%d %d %d]: Exiting after %d threads\n", getpid(), getppid(), gettid(), threads_num);
+    return SUCCESS;
+}
